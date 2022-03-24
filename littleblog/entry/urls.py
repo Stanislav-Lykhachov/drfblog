14a8c5eedfe59e2dc714d43a87e7731e72cee422
2DLenin <stanislav.roasting@gmail.com>
2022-03-24 19:11:58 +0200

    Reformat endpoints
    Nested structures were added to make endpoints consistent.
    All the endpoints were written in "Snake case notation".
    
    Resolves: #2

--- littleblog/entry/urls.py
+++ littleblog/entry/urls.py
@@ -3,9 +3,9 @@ from django.urls import path, include
 from .views import UserProfileCreateView, EntryDetailView, RateUpdateView, EntryCreateView
 
 urlpatterns = [
-    path('CreateUserProfile/', UserProfileCreateView.as_view()),
-    path('EntryDetail/<int:pk>/', EntryDetailView.as_view()),
-    path('EntryCreate/', EntryCreateView.as_view()),
-    path('set_mark/', RateUpdateView.as_view())
+    path('create_profile/', UserProfileCreateView.as_view()),
+    path('entries/<int:pk>/', EntryDetailView.as_view()),
+    path('entries/create/', EntryCreateView.as_view()),
+    path('entries/<int:pk>/rate/', RateUpdateView.as_view())
     ]
 
